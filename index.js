5c5a1c480c2e496636a905938dc84474c00cb560
Dario <gomezdn@outlook.com>
2024-05-29 13:26:27 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { etch } = require('./src/methods/etch/etch.js')
+const { commitTx } = require('./src/methods/etch/commitTx.js')
+const { revealTx } = require('./src/methods/etch/revealTx.js')
+const { isConfirmed, getConfirmations } = require('./src/services/tx.js')
+const { findUtxo } = require('./src/services/utxo.js')
+
+module.exports = {
+  etch,
+  commitTx,
+  revealTx,
+  isConfirmed,
+  getConfirmations,
+  findUtxo
+}
