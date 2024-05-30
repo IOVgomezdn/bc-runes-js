615fb9aa615b64db79e1b47ca1d61931198e5c5e
Dario <gomezdn@outlook.com>
2024-05-30 13:24:28 -0300

    add exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { etch } = require('./src/methods/etch/etch.js')
 const { commitTx } = require('./src/methods/etch/commitTx.js')
 const { revealTx } = require('./src/methods/etch/revealTx.js')
-const { isConfirmed, getConfirmations } = require('./src/services/tx.js')
+const { isConfirmed, getConfirmations, waitForTxToBeConfirmed, waitForTxToMature } = require('./src/services/tx.js')
 const { findUtxo } = require('./src/services/utxo.js')
 
 module.exports = {
@@ -10,5 +10,7 @@ module.exports = {
   revealTx,
   isConfirmed,
   getConfirmations,
+  waitForTxToBeConfirmed,
+  waitForTxToMature,
   findUtxo
 }
